c3c28f2e592a9b975ddbfd107f88a1d36bea8307
MatteoAndriolo <matteo.andriolo.2@studenti.unipd.it>
2024-04-25 20:22:46 +0200

    new datasets

--- src/archetypes.R
+++ src/archetypes.R
@@ -12,6 +12,7 @@ archetypesMelanoma <- function(k){
   save(a,file=paste("/app/out/Melanoma/Archetypes_",k,".rds",sep=""))
 }
 
+# ------------------------------------------------------------------------------
 archetypesMyocardialInfarction <- function(k){
   se=loadMyocardialInfarction()
   m=as.matrix(se@assays$RNA@data)
@@ -25,35 +26,45 @@ archetypesMyocardialInfarction <- function(k){
   save(a,file=paste("/app/out/MyocardialInfarction/Archetypes_",k,".rds",sep=""))
 }
 
+# ------------------------------------------------------------------------------
 archetypesMouseCortex <- function(k){
-#  namefile = paste("out/MouseCortex/Archetypes_",k,sep="")
-#  sinkToFile(paste(namefile,".log",sep=""))
+  se=loadMouseCortex()
+  # se is Seurat4 object
+  m=as.matrix(se@data)
+  
+  s=timestamp()
+  a=archetypes::archetypes(m, k=k, verbose=TRUE, maxIterations=10)
+  e=timestamp()
+  
   cat("Not Implemented")
 #  sinkDefault()
 }
 
+# ------------------------------------------------------------------------------
 archetypesExp1 <- function(k){
-#  namefile = paste("out/AllonKleinLab/Exp1/Archetypes_",k,sep="")
-#  sinkToFile(paste(namefile,".log",sep=""))
-  cat("Not Implemented")
-#  sinkDefault()
+  m=loadExp1()
+  m=as.matrix(m)
+  
+  s=timestamp()
+  a=archetypes::archetypes(m, k=k, verbose=TRUE, maxIterations=10)
+  e=timestamp()
+  
+  save(a,file=paste("/app/out/AllonKleinLab/Experiment1/Archetypes_",k,".rds",sep=""))
 }
 
+# ------------------------------------------------------------------------------
 archetypesExp2 <- function(k){
-#  namefile = paste("out/AllonKleinLab/Exp2/Archetypes_",k,sep="")
-#  sinkToFile(paste(namefile,".log",sep=""))
-  cat("Not Implemented")
-#  sinkDefault()
+  se=loadExp2()
 }
 
+# ------------------------------------------------------------------------------
 archetypesExp3 <- function(k){
-#  namefile = paste("out/AllonKleinLab/Exp3/Archetypes_",k,sep="")
-#  sinkToFile(paste(namefile,".log",sep=""))
-  cat("Not Implemented")
-#  sinkDefault()
+  se=loadExp3()
 }
 
+# ------------------------------------------------------------------------------
 # Function to run Archetype analysis
+# ------------------------------------------------------------------------------
 runArchetypeAnalysis <- function(dataset, numArchetypes) {
   print(paste("Running Archetype analysis on", dataset, "with", numArchetypes, "archetypes"))
 
--- src/loaddata.R
+++ src/loaddata.R
@@ -1,8 +1,9 @@
+require(Matrix)
+require(dplyr)
+require(monocle3)
+  
 # SMALL SNAPSHOT
 loadMelanoma <- function(){
-  library(dplyr)
-  library(Matrix)
-  
   a <- read.table("data/Melanoma/GSE72056_melanoma_single_cell_revised_v2.txt", header = TRUE)
   a=a[!duplicated(a[,1]),]
   
@@ -27,7 +28,6 @@ loadMelanoma <- function(){
 
 # LARGE SNAPSHOT
 loadMyocardialInfarction <- function(){
-  library(Matrix)
   se=readRDS("data/MyocardialInfarction/e61af320-303a-4029-8500-db6636bba0d4.rds")
   return(se)
 }
@@ -41,30 +41,38 @@ loadMouseCortex <- function(){
 
 # LARGE TIMESERIES
 loadExp1 <- function(){
-  requite(Matrix)
-  require(dplyr)
-  require(monocle3)
-  clone_matrix <- Matrix::readMM("data/AllonKleinLab/Experiment1/stateFate_inVitro_clone_matrix.mtx")
-  normed_count <- Matrix::readMM("data/AllonKleinLab/Experiment1/stateFate_inVitro_normed_counts.mtx")
-  cell_metadata <- read.table("data/AllonKleinLab/Experiment1/stateFate_inVitro_metadata.txt",header=TRUE,sep = "\t" )
-  gene_names <- read.table("data/AllonKleinLab/Experiment1/stateFate_inVitro_gene_names.txt")
-  neutrophil_monocyte_trajectory <- read.table("data/AllonKleinLab/Experiment1/stateFate_inVitro_neutrophil_monocyte_trajectory.txt",header=TRUE,sep="\t")
-  neutrophil_pseudotime <- read.table("data/AllonKleinLab/Experiment1/stateFate_inVitro_neutrophil_pseudotime.txt",header=TRUE, sep="\t")
+  # Binary matrix indicating clonal membership of each cell
+  # # The rows of this file represent cells and correspond to the rows of _counts_matrix_in_vitro_ (above).
+  # # The columns represent clones. Not every cell belongs to a clone. 
+  #clone_matrix <- Matrix::readMM("data/AllonKleinLab/Experiment1/stateFate_inVitro_clone_matrix.mtx")
   
-  # TODO create Seurat / Monocle3 + SueratWrapper
-  # as in https://cole-trapnell-lab.github.io/monocle3/docs/getting_started/
+  # Number of transcrits for each gene in each cell
+  normed_count <- Matrix::readMM("data/AllonKleinLab/Experiment1/stateFate_inVitro_normed_counts.mtx")
+  
+  # # cell metadata : cell type annotation
+  # cell_metadata <- read.table("data/AllonKleinLab/Experiment1/stateFate_inVitro_metadata.txt",header=TRUE,sep = "\t" )
+  # 
+  # gene_names <- read.table("data/AllonKleinLab/Experiment1/stateFate_inVitro_gene_names.txt")
+  # 
+  # # List of cells belonging to the neutrophil/monocyte trajectory that were used in becnmark analysis
+  # neutrophil_monocyte_trajectory <- read.table("data/AllonKleinLab/Experiment1/stateFate_inVitro_neutrophil_monocyte_trajectory.txt",header=TRUE,sep="\t")
+  
+  # # pseudotime for neutrophil trajectory cells
+  # neutrophil_pseudotime <- read.table("data/AllonKleinLab/Experiment1/stateFate_inVitro_neutrophil_pseudotime.txt",header=TRUE, sep="\t")
+  
+  return(normed_count)
 }
 
 loadExp2 <- function(){
-  clone_matrix <- Matrix::readMM("data/AllonKleinLab/Experiment2/stateFate_inVivo_clone_matrix.mtx")
-  gene_names <- read.table("data/AllonKleinLab/Experiment2/stateFate_inVivo_gene_names.txt",sep="\t")
-  metadata <- read.table("data/AllonKleinLab/Experiment2/stateFate_inVivo_metadata.txt",sep="\t")
+  # clone_matrix <- Matrix::readMM("data/AllonKleinLab/Experiment2/stateFate_inVivo_clone_matrix.mtx")
+  # gene_names <- read.table("data/AllonKleinLab/Experiment2/stateFate_inVivo_gene_names.txt",sep="\t")
+  # metadata <- read.table("data/AllonKleinLab/Experiment2/stateFate_inVivo_metadata.txt",sep="\t")
   normed_counts <- Matrix::readMM("data/AllonKleinLab/Experiment2/stateFate_inVivo_normed_counts.mtx")
-  
+  return(normed_counts) 
 }
 loadExp3 <- function(){
-  clone_matrix = Matrix::readMM("data/AllonKleinLab/Experiment3/stateFate_cytokinePerturbation_clone_matrix.mtx")
-  gene_names <- read.table( "data/AllonKleinLab/Experiment3/stateFate_cytokinePerturbation_gene_names.txt")
-  metadata <- read.table("data/AllonKleinLab/Experiment3/stateFate_cytokinePerturbation_metadata.txt", sep="\t")
+  # clone_matrix = Matrix::readMM("data/AllonKleinLab/Experiment3/stateFate_cytokinePerturbation_clone_matrix.mtx")
+  # gene_names <- read.table( "data/AllonKleinLab/Experiment3/stateFate_cytokinePerturbation_gene_names.txt")
+  # metadata <- read.table("data/AllonKleinLab/Experiment3/stateFate_cytokinePerturbation_metadata.txt", sep="\t")
   normed_counts <- Matrix::readMM("data/AllonKleinLab/Experiment3/stateFate_cytokinePerturbation_normed_counts.mtx")
 }
