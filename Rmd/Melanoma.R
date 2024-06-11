d676da2c2bbe9391d9d3f68904b7b56495b59ecf
MatteoAndriolo <matteo.andriolo.2@studenti.unipd.it>
2024-06-11 12:45:47 +0200

    working plotting

--- Rmd/Melanoma.R
+++ Rmd/Melanoma.R
@@ -1,19 +1,4 @@
----
-title: "Melanoma"
-author: "MatteoAndriolo"
-output: 
-  html_document: 
-    toc: true
-    toc_depth: 3
-    number_sections: true
----
-
-```{r, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE, warning = FALSE, message = FALSE)
-```
-
-```{r setup}
-# Placed inside Rprofile
+# %% Load necessary libraries
 library(Seurat)
 library(ggplot2)
 library(here)
@@ -25,18 +10,14 @@ if (!require(kneedle)) {
 }
 devtools::install_github("etam4260/kneedle")
 library(kneedle)
-library(cowplot)
 
 HVF <- TRUE
 TEST <- TRUE 
-TEST <- FALSE
 
 TEST_genes <- 300
 TEST_samples <- 500
-```
 
-# Load Dataset
-```{r Load Melanoma dataset, warning=FALSE, error=FALSE, message=FALSE,results='hide'}
+#%% Load Dataset
 data_path <- "../data/Melanoma/GSE72056_melanoma_single_cell_revised_v2.txt"
 out_path <- "../out/Melanoma"
 
@@ -72,52 +53,32 @@ se <- FindVariableFeatures(se)
 
 se <- RunPCA(se, features = VariableFeatures(se))
 se <- RunUMAP(se, features = VariableFeatures(se))
-```
+
 # Visualize data
-
-```{r}
-# Generate the PCA plot
-imgname_pca <- sprintf("%s/PCA.png", out_path)
-message(sprintf("Saving Image --- %s", imgname_pca))
+imgname <- sprintf("%s/PCA.png", out_path)
+message(sprintf("Saving Imagine --- %s", imgname))
 pcaplot <- PCAPlot(se)
+# ggsave(imgname, plot=pcaplot)
+print(pcaplot)
 
-# Generate the UMAP plot
-imgname_umap <- sprintf("%s/UMAP.png", out_path)
-message(sprintf("Saving Image --- %s", imgname_umap))
+imgname <- sprintf("%s/UMAP.png", out_path)
+message(sprintf("Saving Imagine --- %s", imgname))
 umapplot <- UMAPPlot(se)
+# ggsave(imgname, plot=umapplot)
+print(umapplot)
 
-# Combine the two plots using cowplot
-combined_plot <- plot_grid(pcaplot, umapplot, labels = c("A", "B"))
-
-# Save the combined plot
-combined_imgname <- sprintf("%s/Combined_Plots.png", out_path)
-message(sprintf("Saving Image --- %s", combined_imgname))
-ggsave(combined_imgname, plot = combined_plot)
-
-# Display the combined plot
-print(combined_plot)
-
-```
-
-```{r Elbowplot, message=TRUE}
 imgname <- sprintf("%s/elbow.pdf", out_path)
 message(sprintf("Saving Imagine --- %s", imgname))
 elbowplot <- ElbowPlot(se)
 # ggsave(imgname, plot=elbowplot)
-elbowplot
-```
-
+print(elbowplot)
 
 # Archetypes
-```{r AA, message=TRUE, warning=TRUE}
-k <- 5
+k <- 10
 k <- kneedle(elbowplot$data$dims, elbowplot$data$stdev)[1]
-message(sprintf("k=%d", k))
 
 if (HVF) {
   m <- se@assays$RNA@layers$counts[which(se@assays$RNA@meta.data$vf_vst_counts_rank > 0), ]
-  se.large <- se
-  se <- se[which(se@assays$RNA@meta.data$vf_vst_counts_rank > 0), ]
 } else {
   m <- se@assays$RNA@layers$counts
 }
@@ -125,68 +86,55 @@ m <- m[Matrix::rowSums(m) > 0, Matrix::colSums(m) > 0]
 m <- as.matrix(m)
 
 s <- timestamp()
-a <- archetypes::archetypes(m, k = k, verbose = TRUE, maxIterations = 10, saveHistory = TRUE)
+a <- archetypes::archetypes(m, k = k, verbose = TRUE, maxIterations = 10)
 e <- timestamp()
 
-#save(a, file = sprintf("%s/Archetypes_%2d.rds", out_path, k))
-```
+save(a, file = sprintf("%s/Archetypes_%2d.rds", out_path, k))
 
 ## Visualize Archetypes
-```{r Visualize AA, message=TRUE, warning=TRUE}
-imgname <- sprintf("%s/Archetypes_%2d.png", out_path, k)
-plotarchetyps <- xyplot(a, m)
-# ggsave(imgname, plot=plotarchetyps)
-plotarchetyps
-```
+#imgname <- sprintf("%s/Archetypes_%2d.png", out_path, k)
+#plotarchetyps <- xyplot(a, m)
+## ggsave(imgname, plot=plotarchetyps)
+#print(plotarchetyps)
 
-```{r}
-simplexplot(a)
-```
-
-```{r}
-#simplex_projection(a)
-```
-
-## Plot Umap Archetypes
-```{r}
-source("../src/R/tools.R")
-TH_archetypes.umap(se,a,out_path)
-
-```
-```{r}
-umap_result <- UMAPPlot(se)
-umap_data <- as.data.frame(umap_result$data)[,1:2]
+# Visualize UMAP archetypes
+if(!require(umap)){
+  install.packages("umap")
+}
+library(umap)
+library(cowplot)
+umap_result <- umap(m)
+umap_result <- umapplot[[1]]
+umap_data <- as.data.frame(umap_result$layout)
 colnames(umap_data) <- c("UMAP1", "UMAP2")
-threshold <- 0.2
+colnames(umap_data) <- c("umap_2", "umap_2")
 
 # Get the archetype weights
 weights <- coef(a)
 weights <- as.data.frame(weights)
-# Set a minimum threshold
-weights[weights < threshold] <- 0
 
-column_sums <- colSums(a$archetypes)  # Sum of each column
-normalized_mat <- sweep(a$archetypes, 2, column_sums, FUN = "/")  # Divide each element by its column sum
-weights=as.data.frame(normalized_mat)
+# Set a minimum threshold
+threshold <- 0.2
+weights[weights < threshold] <- 0
 
 plot_list <- list()
 # Plotting
 for (i in 1:k) {
-  umap_data$weight <- t(weights[i,])
+  umap_data$weight <- weights[, i]
   plot_title <- sprintf("UMAP Archetype %d", i)
   imgname <- sprintf("%s/UMAP_Archetype_%d.png", out_path, i)
   
-  umap_plot <- ggplot(umap_data, aes(x = UMAP1, y = UMAP2, color = weight)) +
+  umap_plot <- ggplot(umap_data, aes(x = umap_1, y = umap_2, color = weight)) +
     geom_point(size = 1) +
     scale_color_gradient(low = "grey", high = "red") +
     ggtitle(plot_title) +
     labs(color = "Weight")
+  umap_plot
   #ggsave(imgname, plot = umap_plot)
   
   #message(sprintf("Saving Image --- %s", imgname))
   plot_list[[i]] <- umap_plot
 }
-
 # Combine all plots into a single image
 combined_plot <- plot_grid(plotlist = plot_list, ncol = 2)
 
@@ -195,15 +143,15 @@ combined_imgname <- sprintf("%s/UMAP_Combined.png", out_path)
 combined_plot
 ggsave(combined_imgname, plot = combined_plot, width = 12, height = 8)
 message(sprintf("Saving Combined Image --- %s", combined_imgname))
-```
 
-# Seurat
-```{r}
-DimPlot(se, reduction = "umap")
-DimPlot(se, reduction = "umap", group.by = "cell_type")
-```
 
-```{r}
-se <- FindNeighbors(se, dims = 6:15)
-se <- FindClusters(se, resolution = 0.5)
-```
+
+## Seurat
+#DimPlot(se, reduction = "umap")
+#DimPlot(se, reduction = "umap", group.by = "cell_type")
+#
+#se <- FindNeighbors(se, dims = 6:15)
+#se <- FindClusters(se, resolution = 0.5)
+
+
+
