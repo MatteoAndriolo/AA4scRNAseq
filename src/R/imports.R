411179fed6071128cca897b5bfc1f21e3e72ad92
MatteoAndriolo <matteo.andriolo.2@studenti.unipd.it>
2024-06-12 22:32:41 +0200

    removed installation pkg, dockerfile

--- src/R/imports.R
+++ src/R/imports.R
@@ -5,9 +5,5 @@ library(here)
 library(archetypes)
 library(tidyr)
 library(dplyr)
-if (!require(kneedle)) {
-  install.packages("quantmod")
-  devtools::install_github("etam4260/kneedle")
-}
 library(kneedle)
 library(cowplot)
