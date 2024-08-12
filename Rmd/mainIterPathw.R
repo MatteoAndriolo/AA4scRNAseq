de68ebf681e21b24942bc7ba3d0d62d6e07c5276
Matteo Andriolo <matteo.andriolo.2@studenti.unipd.it>
2024-08-12 10:21:10 +0200

    backup iterative main

---
+++ Rmd/mainIterPathw.R
@@ -0,0 +1,206 @@
+# Name: unique
+sink(stdout(), type = "message")
+sink(stdout(), type = "output")
+# Import necessary R scripts
+source("/app/Rmd/imports.R")
+source("/app/Rmd/classes.R")
+
+# Fetch params from environment variables
+params <- list()
+params$classname <- Sys.getenv("CLASSNAME")
+params$debug <- as.logical(Sys.getenv("DEBUG", "FALSE"))
+params$hvf <- as.logical(Sys.getenv("HVF", "FALSE"))
+params$max_iterations <- as.numeric(Sys.getenv("MAX_ITERATIONS", "100"))
+params$num_restarts <- as.numeric(Sys.getenv("NUM_RESTARTS", "10"))
+params$method <- Sys.getenv("METHOD", "archetypal")
+params$nworkers <- as.numeric(Sys.getenv("NWORKERS", "1"))
+params$out_path <- Sys.getenv("OUT_PATH")
+params$pathw <- as.numeric(Sys.getenv("PATHW", "0"))
+params$test <- as.logical(Sys.getenv("TEST", "FALSE"))
+params$test_genes <- as.numeric(Sys.getenv("TEST_GENES", "300"))
+params$test_samples <- as.numeric(Sys.getenv("TEST_SAMPLES", "500"))
+params$init_method <- Sys.getenv("INIT_METHOD", "furthestsum")
+mink <- as.numeric(Sys.getenv("MINK"))
+maxk <- as.numeric(Sys.getenv("MAXK"))
+k <- as.numeric(Sys.getenv("K", 0))
+
+params$rseed <- 2024
+params$path_figures <- file.path(params$out_path, "figures")
+params$path_outdata <- file.path(params$out_path, "data")
+
+if (k != 0) {
+  params$kappas <- k
+} else if (!(is.na(mink) | is.na(maxk))) {
+  params$kappas <- mink:maxk
+} else {
+  stop("ERROR: main | No k or mink&maxk provided")
+}
+
+
+debug <- params$debug
+
+if (params$pathw == 0) {
+  params$pathw <- NULL
+}
+
+plan("multicore", workers = params$nworkers)
+
+################### FIXING PARAMETERS FOR TESTING
+if (FALSE) {
+  params$debug <- TRUE
+  debug <- TRUE
+  params$classname <- "Melanoma"
+  params$pathw <- 1
+  params$pathw <- NULL
+  params$kappas <- 2:3
+  params$test <- TRUE
+  params$nworkers <- 10
+  plan("multicore", workers = params$nworkers)
+  params$num_restarts <- 2
+  params$max_iterations <- 5
+  params$method <- "archetypal"
+  params$init_method <- "furthestsum"
+  params$out_path <- "/dev/null"
+  params$kappas <- 4:8
+}
+
+if(TRUE){
+  params$debug <- TRUE
+  debug <- TRUE
+  params$kappas <- c(7,12)
+  params$method <- "archetypal"
+  params$init_method <- "furthestsum"
+}
+################### END FIXING PARAMETERS FOR PRESENTATION
+
+for(i in 1:(length(pathways)+1)){
+  # CREATE OBJECT -----
+  message("INITT with pathw ",i)
+  obj <- new(params$classname)
+  if(i>length(pathways)){
+    params$pathw <- NULL
+    params$hvf <- TRUE
+  }else{
+    params$pathw <- i
+  }
+  obj <- do.call(obj_updateParams, c(list(obj = obj), params))
+
+  # LOADING DATA -----
+  message("LOG: main | Loading Data")
+  obj <- obj_loadData(obj)
+  saveRDS(obj@se, file.path(params$path_outdata,paste(namePathw[i],".RdS")))
+
+  message("LOG: main | Visualize Data")
+  # obj_visualizeData(obj)
+  # if (!is.null(obj@se.org)) {
+  #   obj_visualizeData(obj, name = "org")
+  # }
+
+  # ARCHETYPAL --------
+  message("LOG: main | Performing Archetypal")
+  obj <- obj_performArchetypal(obj, doparallel = FALSE)
+
+  message("LOG: main | assign AA clusters")
+  obj <- obj_assignArchetypalClusters(obj)
+
+  message("LOG: main | Seurat Clustering")
+  obj <- obj_seuratCluster(obj)
+
+  obj@archetypes$seurat_clusters <- obj@se$seurat_clusters
+  obj@archetypes$ctypes <- obj@se$ctype
+  obj@other$se.metadata <- obj@se@meta.data
+  obj@other$genenames <- rownames(obj@se)
+  obj@other$cellnames <- colnames(obj@se)
+
+  saveRDS(obj@other, file = file.path(obj@params$path_outdata, paste0(namePathw[i],"_metadata.Rds")))
+  saveRDS(obj@archetypes, file.path(params$path_outdata,paste(namePathw[i],"_AA.Rds")))
+}
+stop()
+
+message("LOG: main | assign AA clusters Done")
+obj <- obj_visualizeArchetypal(obj)
+# SEURAT CLUSTERIZATOIN -------------------------------
+
+
+# SAVE OBJECT -----------
+message("LOG: main | Saving objects")
+obj@archetypes$seurat_clusters <- obj@se$seurat_clusters
+obj@archetypes$ctypes <- obj@se$ctype
+obj@other$se.metadata <- obj@se@meta.data
+obj@other$genenames <- rownames(obj@se)
+obj@other$cellnames <- colnames(obj@se)
+
+saveRDS(obj@other, file = file.path(obj@params$path_outdata, "metadata.Rds"))
+saveRDS(obj@archetypes, file = file.path(obj@params$path_outdata, "archetypes.Rds"))
+
+
+# END HERE #############################################################
+
+
+
+
+
+
+
+# PERFORM ARCHETYPES ---------------------------------
+## ARCHETYPES----
+# if (obj@params$method == "archetypes") {
+#  message("LOG: main | Performing Archetypes")
+#  obj <- obj_performArchetypes(obj, doparallel = FALSE)
+#  message("LOG: main | Performing Archetypes Done")
+#
+#  message("LOG: main | assign AA clusters")
+#  obj <- obj_assignArchetypesClusters(obj)
+#  message("LOG: main | assign AA clusters Done")
+#
+#  message("LOG: main | Visualizing Archetypes")
+#  obj <- obj_visualizeArchetypes(obj)
+#  message("LOG: main | Visualizing Archetypes Done")
+#
+#  # Umap Archetypes Plot
+#  message("LOG: main | Umap Archetypes")
+#  obj <- obj_umapArchetypes(obj)
+#  message("LOG: main | Umap Archetypes Done")
+#
+#  # Archetypes Analysis
+#  message("LOG: main | Analysis Archetypes")
+#  obj_analysisArchetypes(obj)
+#  message("LOG: main | Analysis Archetypes Done")
+#  ## ARCHETYPAL -----
+# } else if (obj@params$method == "archetypal") {
+#
+# }
+
+
+# VISUALIZE ------------------------------------------- # Visualize Dataset
+# message("LOG: main | Visualizing Data")
+# obj <- obj_visualizeData(obj)
+# message("LOG: main | Visualizing Data Done")
+#
+## Visualize Archetypes
+# if (obj@params$method == "archetypes") {
+# }
+
+
+# obj@archetypes$aa.bests[[k]]$cluster.id <- apply(weights, 1, which.max)
+# Compare aa_clusters and seurat_clasters with Ident()
+# for( k in names(obj@archetypes$aa.bests)){
+#   message("OUTPUT: main.R | looking at ",k)
+#   obj@compare$aa.orig <- table(obj@archetypes$aa.bests[[k]], obj@se@meta.data$orig.ident)
+#   obj@compare$se.orig <- table(obj@se@meta.data$seurat_clusters, obj@se@meta.data$orig.ident)
+#   obj@compare$aa.se <- table(obj@se@meta.data$aa_clusters, obj@se@meta.data$seurat_clusters)
+# }
+# obj@compare$aa.orig <- table(obj@se@meta.data$aa_clusters, obj@se@meta.data$orig.ident)
+# obj@compare$se.orig <- table(obj@se@meta.data$seurat_clusters, obj@se@meta.data$orig.ident)
+# obj@compare$aa.se <- table(obj@se@meta.data$aa_clusters, obj@se@meta.data$seurat_clusters)
+#
+# # Display this results
+# message("LOG: main | Comparison of clusters")
+# print(obj@compare$aa.orig)
+# print(obj@compare$se.orig)
+# print(obj@compare$aa.se)
+
+# FIXME manage save obj and in particular name has been removed
+# message("LOG: main | Saving Object")
+# obj_saveObj(obj, name = obj@params$name)
+# message("LOG: main | Saving Object Done")
